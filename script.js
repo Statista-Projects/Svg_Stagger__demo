d94b750c98b1935dc9adbcd466a4fa3a7160c489
Statista-Projects-Dev-Team <dun.lewis@statista.com>
2022-10-01 19:13:54 +0000

    Update 2 files

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ let svgStagger = gsap.timeline({
     trigger: ".section-wrapper",
     start: "top top",
     end: "bottom bottom",
-    scrub: true
-  }
+    scrub: true,
+  },
 });
 svgStagger.from(".boxes rect", {
   opacity: 0.3,
   duration: 0.1,
-  stagger: { amount: 0.3, from: "random" }
+  stagger: { amount: 0.3, from: "random" },
 });
 svgStagger.from(".bolt polygon", {
   scale: 0.1,
   stagger: 0.02,
-  ease: "power3.in"
+  ease: "power3.in",
 });
 svgStagger.from(
   ".bolt polygon",
@@ -24,7 +24,7 @@ svgStagger.from(
     opacity: 0,
     duration: 0.3,
     stagger: 0.02,
-    ease: "power3.in"
+    ease: "power3.in",
   },
   "<"
 );
